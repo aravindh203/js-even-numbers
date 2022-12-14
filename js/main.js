5e28935e5b9978eb6d23626726d1458d49cab7c3
aravindh203 <aravindhkumar218@gmail.com>
2022-12-14 11:29:01 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// Display a sequence of even numbers
+
+let sequence=parseInt(prompt("sequence number"));
+for(i=1;i<=sequence;i++){
+    if(i%2==0){
+        document.write(i+"<br>")
+    }
+}
+
